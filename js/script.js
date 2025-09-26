3ab3997d9f478687dfa7bea1ba9575905ec93c99
gustavo portela <portelagustavo947@gmail.com>
2025-09-26 18:42:37 -0300

    add api e arrumando funçao async

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-document.querySelector('.busca').addEventListener('submit',(event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault()
 
     let input = document.querySelector('#searchInput').value
-    if(input !== ""){
+    if (input !== "") {
         showWarning("Carregando...")
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=23d1745cde2f7c1859bf3b9430c2442d&units=metrics&lang=pt_br`
-        let result = await fetch(url)
-    
-    }else{
+        let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=23d1745cde2f7c1859bf3b9430c2442d&units=metric&lang=pt_br`);
 
+        let json = await result.json();
+
+        if (json.cod == 200) {
+            console.log(json)
+        } else {
+            showWarning("Não encontramos essa localização....")
+        }
+
+    } else {
+        showWarning("Digite uma localização...")
     }
-
-
 })
-function showWarning(msg){
+
+function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg
 }
