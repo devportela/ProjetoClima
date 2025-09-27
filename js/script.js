b206d514abf71dfe1950d410114968e435603404
gustavo portela <portelagustavo947@gmail.com>
2025-09-27 05:54:37 -0300

    project clima finalizado 27/09

--- js/script.js
+++ js/script.js
@@ -10,7 +10,15 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
         let json = await result.json();
 
         if (json.cod == 200) {
-            console.log(json)
+            showInfo({
+    name: json.name,
+    country: json.sys.country,
+    temp: json.main.temp,
+    tempIcon: json.weather[0].icon,
+    windSpeed: json.wind.speed,
+    windAngle: json.wind.deg
+});
+
         } else {
             showWarning("Não encontramos essa localização....")
         }
@@ -22,4 +30,19 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg
+}
+
+function showInfo(json){
+showWarning("")
+    document.querySelector('.resultado').style.display = 'block'
+    document.querySelector('.titulo').innerHTML = `${json.name},${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp}<sup>°C</sup>`
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} Km/h`
+
+document.querySelector('.temp img').setAttribute('src',`http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+document.querySelector('.ventoPonto').style.tranform = `rotate(${json.windAngle-90}deg)`
+    
+    
+
+    
 }
